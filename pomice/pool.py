b47f0b118da38fa9ca604a231a1358375c062e1d
Gstone <76681984+Gst0ne@users.noreply.github.com>
2021-11-13 23:07:13 +0530

    Update pool.py

--- pomice/pool.py
+++ pomice/pool.py
@@ -177,7 +177,7 @@ class Node:
                 await asyncio.sleep(retry)
 
                 if not self.is_connected:
-                    await self.connect()
+                    self._bot.loop.create_task(self.connect())
             else:
                 self._bot.loop.create_task(self._handle_payload(msg.json()))
 
